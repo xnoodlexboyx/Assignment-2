80b7a1726d3a89122fcc5ab7a9f6402fbdcff243
Isaac <59423342+xnoodlexboyx@users.noreply.github.com>
2025-05-02 19:43:13 -0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    androidTestImplementation(libs.androidx.uiautomator)
+    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.3.0")
+
 }
